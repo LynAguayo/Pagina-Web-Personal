314704b77fc088aad55257ab6e301241fece3ae4
evelnyit <125038025+evelnyit@users.noreply.github.com>
2025-01-26 22:46:15 +0100

    Script para la animación de texto de la página de inicio

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // --- Menú de navegación ---
-// Gestiona la visibilidad del icono menú de navegación
+// Gestiona la visibilidad del menú de navegación
 
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
@@ -8,3 +8,13 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x'); 
     navbar.classList.toggle('active');  
 };
+
+// --- Animación de texto con Typed.js ---
+// Crea una animación de texto 
+const typed = new Typed('.multiple-text', {
+    strings: ['estudiante de DAM', 'junior developer'], // Frases que aparecerán en la animación
+    typeSpeed: 100,      // Velocidad de escritura
+    backSpeed: 100,      // Velocidad de borrado
+    backDelay: 2000,     // Retraso antes de borrar el texto
+    loop: true           // Repite la animación infinitamente
+});
